2402f1f7c2fd1785f0b9babf129abeff6a3a6d7a
Bhushan Gunjal <Bhushan.Gunjal@ril.com>
2025-09-24 17:21:04 +0530

    Phase 3 Completed - changes made to chat API route

--- API/routes/chat.py
+++ API/routes/chat.py
@@ -1,24 +1,19 @@
 # API/routes/chat.py
 
 from fastapi import APIRouter
-from pydantic import BaseModel
+from API.models.chat_models import ChatRequest, ChatResponse
+from AGENTS.orchestrator import orchestrator
 
 router = APIRouter()
 
-# Request body model
-class ChatRequest(BaseModel):
-    message: str
-
-# Response model (optional, helps with docs)
-class ChatResponse(BaseModel):
-    reply: str
 
 @router.post("/send", response_model=ChatResponse)
-def send_message(request: ChatRequest):
+async def chat_endpoint(request: ChatRequest):
     """
     Endpoint to handle chat messages.
     Right now it just echoes back the user's message.
     Later you can connect this to LangGraph or an LLM.
     """
     user_message = request.message
-    return {"reply": f"You said: {user_message}"}
+    reply = orchestrator(user_message)
+    return ChatResponse(reply=reply)
